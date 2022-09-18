8e225a13080eb745a28e8de122044834683d4bd1
lualzugaray <98998748+lualzugaray@users.noreply.github.com>
2022-09-18 14:33:56 -0300

    Add files via upload
    grupal terminado

--- Trabajo Grupal 3/buscador_de_img_nasa/js/space.js
+++ Trabajo Grupal 3/buscador_de_img_nasa/js/space.js
@@ -26,21 +26,18 @@ document.getElementById("btnBuscar").addEventListener("click",function(){
 function showList(array){
 
     array.forEach(element => {
-        
-       
             
             var elementHTML = `
           
-        
-        <div class="col-md-4">
-            
-        <img class="bd-placeholder-img card-img-top" src="` + element.links[0].href + `"  class="img-thumbnail">
-
-        <h4 class="m-3" align="center">`+ element.data[0].title +`</h4> 
-        <p class="m-2"> `+ element.data[0].description +`</p>
-        <small class="text-muted" >` + element.data[0].date_created + ` </small> 
-    
-        </div>
+            <div class="card-deck">
+            <div class="card">
+              <img class="card-img-top" src="` + element.links[0].href + `"  alt="Card image cap">
+              <div class="card-body">
+              <h4 class="m-3" align="center">`+ element.data[0].title +`</h4> 
+              <p class="m-2"> `+ element.data[0].description +`</p>
+              <small class="text-muted" >` + element.data[0].date_created + ` </small> 
+              </div>
+            </div>
 
         `
        
